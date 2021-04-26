4101cdf99d6d3ab0504741e594ca7f42279df549
mariamodat <mariamodat0@gmail.com>
2021-04-26 18:55:52 +0300

    ok

--- src/components/hornedBeast.js
+++ src/components/hornedBeast.js
@@ -4,10 +4,10 @@ class HornedBeast extends React.Component {
 
     return (
       <div>
-        <h2>{this.props.name}</h2>
-
-        <img src={this.props.url} />
-        <p>{this.props.description}</p>
+        <img src={this.props.img} alt="the img"/>
+        <h2> {this.props.title}</h2>
+        <p> {this.props.disc}</p>
+        <h2>{this.props.count}</h2>
       </div>
     );
   }
