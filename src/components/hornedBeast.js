cbe50f8e86127f85a970420501df9f20180a469a
mariamodat <mariamodat0@gmail.com>
2021-04-26 17:23:40 +0300

    y

--- src/components/hornedBeast.js
+++ src/components/hornedBeast.js
@@ -1,5 +1,7 @@
 import React from 'react';
 class HornedBeast extends React.Component {
+
+
   render() {
 
     return (
--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import HornedBeast from './hornedBeast';
 class Main extends React.Component {
+
+  constructor (props)
+  {
+    super (props);
+  }
+
+
   render() {
+
     return (
       <div>
         <HornedBeast name={'UniWhal'} />
